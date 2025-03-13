08bdf8281da4f3e00617700e881eaadd6b7983dd
Alessandro Sikora <lssndrsikora@gmail.com>
2025-03-13 17:51:12 -0300

    Commit initial files

---
+++ index.js
@@ -0,0 +1,82 @@
+let homePoints = document.getElementById("Hpoints")
+let homeCount=0
+let guestPoints = document.getElementById("Gpoints")
+let guestCount=0
+/* let totalHome = document.getElementById("scoreHome")
+let totalGuest = document.getElementById("scoreGuest") */
+
+
+
+
+
+lehomeCountnt homeCount
+homeCountcounthomeCount
+function saveHpoints1() {
+  
+  homeCount = homeCount + 1
+  homePoints.innerText = homeCount
+  homeCounthomeCount
+  homePoints.innerText = homeCount
+
+function saveHpoints2() {
+  homeCount = homeCount + 2
+  homePoints.innerText = homeCount
+  
+}homeCounthomeCount
+homeCountcounthomeCount
+
+function saveHpoints3() {
+  homeCount = homeCount + 3
+  homePoints.innerText = homeCount
+  
+}
+
+
+
+/*function parse() {
+  count = count
+  HomePoints.innerText = ccount
+}  */
+
+/*
+function scoreHome() {
+  count = count
+  totalHome.innerText = cocount
+}  */
+
+//--------------------------------------------
+ 
+
+function saveGpoints1() {
+  count = count + 1;
+  guestPoints.innerText = count
+  
+}
+
+function saveGpoints2() {
+  count = count + 2;
+  guestPoints.innerText = count
+  
+}
+
+function saveGpoints3() {
+  count = count + 3;
+   guestPoints.innerText = count
+  
+}
+
+
+
+/*
+function parse1() {
+  count = 0;
+  GuestPoints.innerText = count
+} 
+  */
+ 
+ 
+ /*
+ function scoreGuest() {
+  count = 0
+  totalGuest.innerText = count
+} */
